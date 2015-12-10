41895601c94c4cddc0a275e54c79545662b2811c
weixiangbin <317505615@qq.com>
2015-12-10 15:19:53 +0800

    delet

--- younongBack/app/scripts/appLogin/controllers/loginController.js
+++
@@ -1,8 +0,0 @@
-define(['common/controllers', 'appLogin/login_services'],
-    function(controllers) {
-        controllers.controller('RootCtrl', ['$scope', 'UserService',
-            function($scope, UserService) {
-                $scope.name = UserService.getUser();
-            }
-        ]);
-    });
